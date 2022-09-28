2b4e5b06f3c265de2a79047127e28479f00172e3
SAPikachu <sapikachu@gmail.com>
2022-09-28 20:32:13 +0800

    Use admin credential for local calls

--- src/local.ts
+++ src/local.ts
@@ -1,12 +1,17 @@
 import { server } from "./server";
 
-server.receive({ jsonrpc: "2.0", id: "1", method: process.argv[2], params: JSON.parse(process.argv[3]) }).then(
-  (res) => {
-    console.log(res?.error || res?.result);
-    process.exit(res?.error ? 1 : 0);
-  },
-  (e) => {
-    console.error(e);
-    process.exit(1);
-  }
-);
+server
+  .receive(
+    { jsonrpc: "2.0", id: "1", method: process.argv[2], params: JSON.parse(process.argv[3]) },
+    { context: { user: { sub: "grindery:internal:local", workspace: "ADMIN", role: "admin" } } }
+  )
+  .then(
+    (res) => {
+      console.log(res?.error || res?.result);
+      process.exit(res?.error ? 1 : 0);
+    },
+    (e) => {
+      console.error(e);
+      process.exit(1);
+    }
+  );
