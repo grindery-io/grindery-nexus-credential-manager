0ce784709c4f3aed17b4f15d8e4040cc370ddd3f
SAPikachu <sapikachu@gmail.com>
2022-10-07 19:51:34 +0800

    Allow using custom token

--- src/local.ts
+++ src/local.ts
@@ -1,17 +1,23 @@
+import { parseUserAccessToken } from "./jwt";
 import { server } from "./server";
 
-server
-  .receive(
+(async () =>
+  server.receive(
     { jsonrpc: "2.0", id: "1", method: process.argv[2], params: JSON.parse(process.argv[3]) },
-    { context: { user: { sub: "grindery:internal:local", workspace: "ADMIN", role: "admin" } } }
-  )
-  .then(
-    (res) => {
-      console.log(res?.error || res?.result);
-      process.exit(res?.error ? 1 : 0);
-    },
-    (e) => {
-      console.error(e);
-      process.exit(1);
+    {
+      context: {
+        user: process.env.TOKEN
+          ? await parseUserAccessToken(process.env.TOKEN)
+          : { sub: "grindery:internal:local", workspace: "ADMIN", role: "admin" },
+      },
     }
-  );
+  ))().then(
+  (res) => {
+    console.log(res?.error || res?.result);
+    process.exit(res?.error ? 1 : 0);
+  },
+  (e) => {
+    console.error(e);
+    process.exit(1);
+  }
+);
