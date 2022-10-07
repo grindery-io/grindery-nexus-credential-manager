0b6f7740809b6f5a7e3907748542d0f456c1413c
SAPikachu <sapikachu@gmail.com>
2022-10-07 19:52:10 +0800

    Use environment variable for key URL

--- src/jwt.ts
+++ src/jwt.ts
@@ -23,7 +23,9 @@ type AccessTokenExtra =
     };
 export type TAccessToken = TypedJWTPayload<AccessTokenExtra>;
 
-const ORCHESTRATOR_KEY = jose.createRemoteJWKSet(new URL("https://orchestrator.grindery.org/oauth/jwks"));
+const ORCHESTRATOR_KEY = jose.createRemoteJWKSet(
+  new URL(process.env.ORCHESTRATOR_PUBLIC_KEY || "https://orchestrator.grindery.org/oauth/jwks")
+);
 export async function parseUserAccessToken(token: string): Promise<TAccessToken> {
   const { payload } = await jose.jwtVerify(token, ORCHESTRATOR_KEY, {
     issuer: "urn:grindery:orchestrator",
